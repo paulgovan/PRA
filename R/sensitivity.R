c0322dce5c5f5c0dbb7889d6c1da5b8632b2e920
Paul Govan <paul.govan2@gmail.com>
2026-01-31 13:45:20 -0500

    Update mcs

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,9 +1,21 @@
 #' Sensitivity Analysis.
 #'
-#' @param task_dists A list of lists describing each task distribution.
-#' @param cor_mat The correlation matrix for the tasks (Optional).
+#' This function performs sensitivity analysis on a project with multiple tasks,
+#' each having its own cost distribution. It calculates the sensitivity of the
+#' variance in total project cost with respect to the variance in each task's cost.
+#' It can also account for correlations between task costs if a correlation matrix
+#' is provided.
+#'
+#' @param task_dists A list of lists describing each task distribution. Each inner
+#' list should contain the type of distribution and its parameters. Supported distributions
+#' are "normal", "triangular", and "uniform".
+#' @param cor_mat The correlation matrix for the tasks (Optional). If provided, it
+#' should be a square matrix with dimensions equal to the number of tasks. If not
+#' provided, tasks are assumed to be independent.
 #' @return The function returns a vector of sensitivity results with respect to
-#' each task.
+#' each task. Each element in the vector corresponds to the sensitivity of the variance
+#' in total project cost with respect to the variance in the respective task's cost.
+#'
 #' @examples
 #' # Set the task distributions for a toy project.
 #' task_dists <- list(
@@ -33,6 +45,26 @@
 #'   height = data$Sensitivity, names = data$Tasks, col = "skyblue",
 #'   horiz = TRUE, xlab = "Sensitivity", ylab = "Tasks"
 #' )
+#' title("Sensitivity Analysis of Project Tasks")
+#'
+#' # Without correlation matrix
+#' sensitivity_results_indep <- sensitivity(task_dists)
+#' cat("Sensitivity of the variance in total cost with respect to the variance in each task
+#' cost (independent tasks):\n")
+#' print(sensitivity_results_indep)
+#'
+#' # Build a vertical barchart and display the results.
+#' data_indep <- data.frame(
+#'  Tasks = c("A", "B", "C"),
+#'  Sensitivity = sensitivity_results_indep
+#'  )
+#'  barplot(
+#'  height = data_indep$Sensitivity, names = data_indep$Tasks,
+#'  col = "lightgreen",
+#'  horiz = TRUE, xlab = "Sensitivity", ylab = "Tasks"
+#'  )
+#'  title("Sensitivity Analysis of Project Tasks (Independent)")
+#'
 #' @export
 
 # Define the sensitivity analysis function
@@ -82,3 +114,5 @@ sensitivity <- function(task_dists, cor_mat = NULL) {
   # Return the sensitivity vector
   return(sensitivity)
 }
+
+
